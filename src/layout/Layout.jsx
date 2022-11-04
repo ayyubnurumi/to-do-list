c5b324038997cc0070c07e1a4c63066ae273272e
ayyubnurumi <ayyubinvestment@gmail.com>
2022-11-04 21:59:22 +0700

    to-do-list

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,14 +3,16 @@ import { Outlet } from 'react-router'
 import './Layout.css'
 
 export const Layout = () => {
+  const logout = () => localStorage.removeItem("userCredentials");
   return (
     <div id='body'>
         <header id='header'>
           <h1 id='title'><a className='link' href='home'>to-do-list ayyub</a></h1>
           <select>
-            <option></option>
+            <option><a className='link account' href='profile'>account</a></option>
+            <option><a href='login' onClick={logout}>logout</a></option>
           </select>
-          <a className='link account' href='profile'>account</a>
+          
         </header>
         <Outlet />
         <footer id='footer'>made by ayyub</footer>
